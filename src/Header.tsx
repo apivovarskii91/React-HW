826ccb4d2423454e30451273d82e8085c56f051c
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-24 15:50:34 +0200

    remove files

--- src/Header.tsx
+++
@@ -1,6 +0,0 @@
-type HeaderProps = {
-    title: string
-}
-export const Header = ({ title }: HeaderProps) => {
-    return <h1>Helow {title}</h1>
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Header } from './Header'
-import Content from './Content'
-import List from './List'
 
-const App = () => {
-    return (
-        <>
-            <Header title="React" />
-            <Content
-                text1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio fugit est explicabo provident laboriosam distinctio! Ratione excepturi voluptatem eos blanditiis soluta qui iste, quam voluptatum dolor ipsum recusandae ad minima!"
-                text2="Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio fugit est explicabo provident laboriosam distinctio! Ratione excepturi voluptatem eos blanditiis soluta qui iste, quam voluptatum dolor ipsum recusandae ad minima!"
-            />
-            <List item="App" item2="React" item3="TS" />
-        </>
-    )
-}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
