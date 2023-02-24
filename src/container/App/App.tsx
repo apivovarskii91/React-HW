4bff03628607e21e7fb5d2498f2c99132f72ba43
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-24 18:15:08 +0200

    Header logo

---
+++ src/container/App/App.tsx
@@ -0,0 +1,19 @@
+import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
